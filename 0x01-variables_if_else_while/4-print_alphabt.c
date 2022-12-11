3bcc9eb370b7622178da37f6b52e2be377b5b3ba
AMRExcel <anthonymoboluwaduro@gmail.com>
2022-12-11 04:30:20 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c l= 'e' && c l= 'q')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
